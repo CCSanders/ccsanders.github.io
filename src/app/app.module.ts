22482e2ade47ea20703f294a72bc566c6cb754be
Colin Sanders <ck.sanders4@gmail.com>
2020-05-29 10:47:18 -0500

    final package update for now, hopefully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatSidenavModule, MatButtonModule, MatListModule, MatIconModule} from "@angular/material"
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 //3rd party modules
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
