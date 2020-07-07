7a816aacb4a3e10c94883fc91c5c78a49a3efc14
Colin Sanders <ck.sanders4@gmail.com>
2020-07-07 14:45:05 -0500

    fixed img path for mobile in blog service

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -13,7 +13,7 @@ private data : BlogArticleModel[] = new Array(
 	title: "My New PC: The ITX Monster",
 	summary: "I have finally completed a long-time dream of mine: A DIY mini-ITX PC that has absolutely monster capabilities. Overview of the specs: i7 10700K, RTX 2080 Super, 32GB RAM, 1TB 970 Evo / 2TB HDD, Water Cooled, RGB, and most importantly: it fits right on my desk!",
 	mdSrc: "/assets/md/pc-build.md",
-	headerImgUrl: "/assets/imgs/keyboard-banner.png",
+	headerImgUrl: "assets/imgs/keyboard-banner.png",
 	author: "Colin Sanders",
 	date: "June 28",
 	length: 5,
